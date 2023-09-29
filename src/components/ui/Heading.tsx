dc06cf3688b96506a570622fbe309176747e674a
Sofia Ferreira <sofia.ferreira@impactcommerce.pt>
2023-09-29 18:16:47 +0100

    change to typescript

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,4 +1,10 @@
-export default function Heading({ title, subtitle }) {
+interface HeadingProps {
+    title: string,
+    subtitle?: string
+}
+
+export default function Heading(props: HeadingProps) {
+    const {title, subtitle} = props
     return (
         <>
             <h1 className="text-center text-2xl mt-10">{title}</h1>
